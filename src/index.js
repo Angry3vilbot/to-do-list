6e39dfa66acf146a3ea601ca10e669c0415632dd
Angry3vlibot <myh.balaker@gmail.com>
2022-10-30 00:02:14 +0300

    Add due date and description to cards

--- src/index.js
+++ src/index.js
@@ -18,16 +18,20 @@ function hideForm(){
 
 function submitEntry(){
     let n = document.getElementById('name')
+    let desc = document.getElementById('description')
+    let date = document.getElementById('date')
     let importance
     const radioButtons = document.querySelectorAll('input[name="importance"]');
     n = n.value
+    desc = desc.value
+    date = date.value
     for (const sel of radioButtons){
         if (sel.checked) {
             importance = sel.value;
             break;
         }
     }
-    return [n, importance]
+    return [n, desc, date, importance]
 }
 function changeState(){
     let parent = this.parentElement
@@ -49,20 +53,28 @@ function changeState(){
 function createEntry(){
    let variables = submitEntry()
    let n = variables[0]
-   let importance = variables[1]
+   let desc = variables[1]
+   let due = variables[2]
+   let importance = variables[3]
    console.log(`${n} ||| ${importance}`)
    let div = document.createElement('div')
    let h3 = document.createElement('h3')
+   let des = document.createElement('p')
    let p = document.createElement('p')
+   let date = document.createElement('p')
    let btn = document.createElement('button')
+   date.classList.add('task-duedate')
+   des.classList.add('task-description')
    h3.classList.add('task-title')
    p.classList.add(importance)
    div.classList.add('task-card')
+   des.innerHTML = desc
+   date.innerHTML = `Due: ${due}`
    h3.innerHTML = n
    p.innerHTML = `Importance - <mark>${importance}</mark>`
    btn.innerHTML = 'Move to "In progress"'
    btn.addEventListener('click', changeState)
-   div.append(h3, p, btn)
+   div.append(h3, des, date, p, btn)
    todoContainer.append(div)
    hideForm()
 }
